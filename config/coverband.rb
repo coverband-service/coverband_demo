89073f939aab814c46d63b139fbc65fcc56916c7
Dan Mayer <dan.mayer@offgrid-electric.com>
2018-08-07 21:02:05 -0600

    add redis url

--- config/coverband.rb
+++ config/coverband.rb
@@ -1,11 +1,17 @@
 Coverband.configure do |config|
   config.root              = Dir.pwd
   config.collector         = 'coverage'
-  config.redis             = Redis.new() if defined? Redis
-  # TODO NOTE THIS IS A ISSUE IN THE 2.0 release you set something like redis and the store
-  # I need to look a bit more at this but got one bug report quickly after release
-  # (even though test builds didnt need)
-  config.store             = Redis.new() if defined? Redis
+  if ENV['REDIS_URL']
+    config.redis             = Redis.new(url: ENV['REDIS_URL'])
+    # TODO NOTE THIS IS A ISSUE IN THE 2.0 release you set something like redis and the store
+    # I need to look a bit more at this but got one bug report quickly after release
+    # (even though test builds didnt need)
+    config.store             = Redis.new(url: ENV['REDIS_URL'])
+  else
+    config.redis             = Redis.new()
+    config.store             = Redis.new()
+  end
+
   config.ignore            = %w[vendor .erb$ .slim$]
   # add paths that you deploy to that might be different than your local dev root path
   config.root_paths        = []
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_08_02_195248) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
