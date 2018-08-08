12f02c2767bd8d9f8d496e4aa269268a1313977c
Dan Mayer <dan.mayer@offgrid-electric.com>
2018-08-07 20:50:16 -0600

    fix typo

--- app/controllers/unreachable_controller.rb
+++ app/controllers/unreachable_controller.rb
@@ -2,7 +2,7 @@
 # It is hear to show that code coverage on it can't be reached.
 class UnreachableController < ApplicationController
   def index
-    100.times.do
+    100.times do
       Rails.logger.info "you can't get here"
     end
   end
