75e057649f1759769159de6a683605f6f88d0dde
Dan Mayer <dan.mayer@offgrid-electric.com>
2018-08-08 21:49:40 -0600

    add bootstrap material

--- README.md
+++ README.md
@@ -10,11 +10,15 @@ The demo site is hosted on Heroku.
 
 * basic setup was done following the standard [Heroku Rails 5 Guide](https://devcenter.heroku.com/articles/getting-started-with-rails5)
 
+# Theme
+
+The initial design off the demo site was pulled from a demo'ed theme, a Material Design Bootstrap 4 Theme.
+
+* [From Rails 5 to Bootstrap 4 — Responsive Admin Dashboard Template](https://medium.com/@frontted/from-rails-5-to-bootstrap-4-responsive-admin-dashboard-template-1de103c6216c)
+* [Hero Rails](https://github.com/frontted/hero-rails)
+
 # TODO
 
-* add deploy to heroku button support
-* pick a theme / design perhaps
-   * Material design https://github.com/seanpaulconnelly/rails5-material-design
-   * bootstrap? https://medium.com/@frontted/from-rails-5-to-bootstrap-4-responsive-admin-dashboard-template-1de103c6216c
+* add deploy to Heroku button support
 * add background job example / integration
 * add cron example / integration 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,39 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require activestorage
 //= require turbolinks
-//= require_tree .
+
+//= require jquery/dist/jquery.min.js
+//= require popper.js/dist/umd/popper.js
+//= require bootstrap/dist/js/bootstrap.min.js
+
+//= require dom-factory/dist/dom-factory.js
+//= require material-design-kit/dist/material-design-kit.js
+
+
+  // Self Initialize DOM Factory Components
+  domFactory.handler.autoInit()
+
+  // Connect button(s) to drawer(s)
+  var sidebarToggle = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="sidebar"]'))
+
+
+  sidebarToggle.forEach(function (toggle) {
+    toggle.addEventListener('click', function (e) {
+      var selector = e.currentTarget.getAttribute('data-target') || '#default-drawer'
+      var drawer = document.querySelector(selector)
+      if (drawer) {
+        drawer.mdkDrawer.toggle()
+      }
+    })
+  })
+
+
+  $('[data-toggle="tooltip"]').tooltip({ container: 'body' });
+  $('[data-toggle="popover"]').popover();
+
+  $('.dropdown.notifications ul a.nav-link').click(function (e) {
+    e.stopPropagation();
+    $(this).tab('show');
+  });
+
+document.addEventListener("turbolinks:load", function() {
+  domFactory.handler.upgradeAll()
+})
