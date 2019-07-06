fe0ecaac21183c6095696cb155616fe7635ff674
Dan Mayer <dan.mayer@stitchfix.com>
2019-07-06 14:27:57 -0600

    links and filter

--- config/initializers/data_tracer.rb
+++ config/initializers/data_tracer.rb
@@ -70,10 +70,13 @@ if ENV['DATA_TRACER']
     config.async = lambda do |event|
       event = Raven.send_event(event)
 
-      #link_to it via https://sentry.io/api/0/organizations/coverband-demo/issues/?limit=25&project=1497449&query=28d935d10f8a4084b3511b4baa958046&shortIdLookup=1&statsPeriod=14d
+      # link_to it via https://sentry.io/api/0/organizations/coverband-demo/issues/?limit=25&project=1497449&query=28d935d10f8a4084b3511b4baa958046&shortIdLookup=1&statsPeriod=14d
       err.backtrace.each do |line|
         err_path = line.split(":").first
         lineno = line.split(":")[1]
+
+        # filter none app code
+        next unless err_path.start_with?(current_root)
 
         file_data[err_path][lineno]['exception_traces'] = [] unless file_data[err_path][lineno]['exception_traces']
         file_data[err_path][lineno]['exception_traces'] << event.id unless (file_data[err_path][lineno]['exception_traces'].length > 5 || file_data[err_path][lineno]['exception_traces'].include?(event.id))
