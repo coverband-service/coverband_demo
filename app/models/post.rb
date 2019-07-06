ba2170191ab653cd69a0d73521cf05430c62e52e
Dan Mayer <dan.mayer@stitchfix.com>
2019-07-06 15:15:35 -0600

    improve test examples

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,19 +3,17 @@ class Post < ApplicationRecord
   # example method to show multiple call paths
   # logic in a method, that can be traced
   #
-  # this is not an example of good code ;)
+  # This is not an example of good code, just a quick way to highlight some functionality ;)
   ###
   def self.clear_bad_posts(all: false, dangerous: false)
     posts = Post.all
     bad_posts = posts.select { |post| post.title.blank? || post.body.blank? }
     if all
-      bad_posts.destroy_all!
+      bad_posts.map(&:destroy!)
+    elsif dangerous
+      bad_posts.first.destroy!
     else
-      if dangerous
-        bad_posts.first.destroy!
-      else
-        bad_posts.first&.destroy!
-      end
+      bad_posts.first&.destroy!
     end
   end
 
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,25 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'clear_bad_posts clears all bad posts' do
+    Post.create!(title: 'bad post', body: '')
+    Post.create!(title: 'bad post2', body: '')
+    original = Post.all.count
+    Post.clear_bad_posts(all: true)
+    assert_equal (original - 2), Post.all.count
+  end
+
+  test 'clear_bad_posts clears one bad post' do
+    Post.create!(title: 'bad post', body: '')
+    Post.create!(title: 'bad post2', body: '')
+    original = Post.all.count
+    Post.clear_bad_posts(all: false)
+    assert_equal (original - 1), Post.all.count
+  end
+
+  test 'clear_bad_posts dangerously can throw exceptions' do
+    assert_raise NoMethodError do
+      Post.clear_bad_posts(dangerous: true)
+    end
+  end
 end
