9c8e03da405053bc4238ebc4ecf9597a2afdce17
Dan Mayer <dan.mayer@stitchfix.com>
2019-07-06 15:15:35 -0600

    improve test examples

--- config/coverband.rb
+++ config/coverband.rb
@@ -53,5 +53,5 @@ Coverband.configure do |config|
   # true and debug can give helpful and interesting code usage information
   # they both increase the performance overhead of the gem a little.
   # they can also help with initially debugging the installation.
-  config.verbose = true
+  config.verbose = false # true
 end
--- config/initializers/test_tracer.rb
+++ config/initializers/test_tracer.rb
@@ -5,23 +5,15 @@ if ENV['TEST_TRACER']
   file = './tmp/test_file_data.json'
 
   current_test = nil
-  tests = {}
   file_data = {}
 
   puts 'starting tracer'
   call_trace = TracePoint.new(:call) do |tp|
     if tp.path.start_with?(current_root)
       if tp.defined_class.to_s.match(/Test/)
-        #if current_test && current_test != "#{tp.defined_class}\##{tp.method_id}"
-        #end
         current_test = "#{tp.defined_class}\##{tp.method_id}"
-        puts "setting current_test #{current_test}"
-      elsif current_test
-        #tests[current_test] = [] unless tests.key?(current_test)
-        #tests[current_test] = (tests[current_test] + ["#{tp.defined_class}#{tp.method_id}"])
+        # puts "setting current_test #{current_test}"
       end
-      # puts  [tp.path, tp.lineno, tp.defined_class, tp.method_id, tp.event]
-      # puts tp.inspect
     end
   end
   call_trace.enable
@@ -32,19 +24,17 @@ if ENV['TEST_TRACER']
         file_data[tp.path] = {} unless file_data[tp.path]
         file_data[tp.path][tp.lineno] = {} unless file_data[tp.path][tp.lineno]
         file_data[tp.path][tp.lineno]['invoked_by_test'] = [] unless file_data[tp.path][tp.lineno]['invoked_by_test']
-        file_data[tp.path][tp.lineno]['invoked_by_test'] << current_test unless file_data[tp.path][tp.lineno]['invoked_by_test'].include?(current_test)
+        unless file_data[tp.path][tp.lineno]['invoked_by_test'].include?(current_test)
+          file_data[tp.path][tp.lineno]['invoked_by_test'] << current_test
+        end
       end
-      # puts  [tp.path, tp.lineno, tp.defined_class, tp.method_id, tp.event]
-      # puts tp.inspect
     end
   end
   line_trace.enable
 
   at_exit do
-    # puts "mapped_tests: "
-    # puts tests
-    puts "file data: "
+    puts 'mapped files to tests: '
     puts file_data
-    File.open(file, 'w') {|f| f.write(file_data.to_json) }
+    File.open(file, 'w') { |f| f.write(file_data.to_json) }
   end
 end
