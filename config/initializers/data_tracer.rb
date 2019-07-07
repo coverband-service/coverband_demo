bb867ffc2c51923b77f1db197415e194119e8338
Dan Mayer <dan.mayer@stitchfix.com>
2019-07-07 14:17:08 -0600

    global

--- config/initializers/data_tracer.rb
+++ config/initializers/data_tracer.rb
@@ -4,6 +4,7 @@ if ENV['DATA_TRACER']=='true'
   current_root = Dir.pwd
   file = './tmp/data_file_data.json'
 
+  $current_exception = nil
   current_trace = nil
   previous_return = nil
   current_dump = nil
@@ -90,7 +91,7 @@ if ENV['DATA_TRACER']=='true'
               # We have to convert to a JSON-like hash, because background job
               # processors (esp ActiveJob) may not like weird types in the event hash
               # configuration.async.call(evt.to_json_compatible)
-              @@current_exception = obj if message_or_exc == "exception"
+              $current_exception = obj if message_or_exc == "exception"
               configuration.async.call(evt)
             rescue => ex
               logger.error("async event sending failed: #{ex.message}")
@@ -120,8 +121,8 @@ if ENV['DATA_TRACER']=='true'
       # event.respond_to?(:backtrace) ? event.backtrace : event_response['exception']['values'][0]['stacktrace']
 
       # link_to it via https://sentry.io/api/0/organizations/coverband-demo/issues/?limit=25&project=1497449&query=28d935d10f8a4084b3511b4baa958046&shortIdLookup=1&statsPeriod=14d
-      if @@current_exception
-        @@current_exception.backtrace.each do |line|
+      if $current_exception
+        $current_exception.backtrace.each do |line|
           err_path = line.split(':').first
           lineno = line.split(':')[1]
 
