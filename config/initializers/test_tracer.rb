fbb2d3ef3a35b064a6466d294efa26a473a46ce0
Dan Mayer <danmayer@gmail.com>
2019-08-03 16:25:12 -0600

    filter vendor

--- config/initializers/test_tracer.rb
+++ config/initializers/test_tracer.rb
@@ -19,7 +19,7 @@ if ENV['TEST_TRACER'] && ENV['TEST_TRACER'] != 'false'
   call_trace.enable
 
   line_trace = TracePoint.new(:line) do |tp|
-    if tp.path.start_with?(current_root) && !tp.path.include?('test')
+    if tp.path.start_with?(current_root) && !tp.path.include?('test') && !tp.path.include?('vendor')
       if current_test
         file_data[tp.path] = {} unless file_data[tp.path]
         file_data[tp.path][tp.lineno] = {} unless file_data[tp.path][tp.lineno]
